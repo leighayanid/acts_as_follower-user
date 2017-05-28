0f3bc1b43e98e47abee2d0db6c24dcc14b4a13fb
Leigh Dinaya <leighdinaya04@gmail.com>
2017-05-28 17:17:12 +0800

    change root route and display users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/show'
-
   get 'home/index'
-  root 'home#index'
+  root 'users#index'
   devise_for :users
+  resource :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

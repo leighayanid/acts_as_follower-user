d5071f8b8824a63780e1b93483f31400d153f1e2
Leigh Dinaya <leighdinaya04@gmail.com>
2017-05-29 08:10:28 +0800

    fix resource route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'users#index'
   devise_for :users
-  resource :users, only: [:index, :show]
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

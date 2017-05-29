77db21c8b5e2e659e287c7eda928f569565df710
Leigh Dinaya <leighdinaya04@gmail.com>
2017-05-29 09:07:59 +0800

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'follows/index'
+
+  get 'home/index'
+  root 'users#index'
+  devise_for :users, controllers: {}
+  resources :users, only: [:index, :show] do
+		resources :follows, only: [:create, :destroy]
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
